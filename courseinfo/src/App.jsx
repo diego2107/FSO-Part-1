c2688bd34ca3ceb9d6d09b3d44b0e118e6194f16
diego2107 <diegotabo2003@hotmail.com>
2025-11-13 10:01:19 -0500

    Exercise 1.4 complete
    Passed an array into components to then render on the screen.

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,38 +1,45 @@
 const App = () => {
   const course = 'Half Stack application development'
-  console.log(course)
-
-  const part1 = {
+  const parts = [
+    {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  console.log(part1.name)
-  console.log(part1.exercises)
-
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  console.log(part2.name)
-  console.log(part2.exercises)
-
-  const part3 = {
+  },
+  {  
     name: 'State of a component',
     exercises: 14
   }
-  console.log(part3.name)
-  console.log(part3.exercises)
-
-  const result = (part1.exercises + part2.exercises + part3.exercises)
-  console.log(result)
+]
   
+  const Header = (props) => {
+    console.log(props)
+    return <h1>{props.course}</h1>
+  }
+
+  const Content = (props) => {
+    console.log(props)
+    return <><p>{props.parts[0].name}: {props.parts[0].exercises}</p>
+             <p>{props.parts[1].name}: {props.parts[1].exercises}</p>
+             <p>{props.parts[2].name}: {props.parts[2].exercises}</p>
+          </>
+  }
+
+  const Total = (props) => {
+    console.log(props)
+    const exercises = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+    return <> Number of exercises: {exercises}
+    </>
+  }
+
   return (
     <div>
-      <p>{course}</p>
-      <p>{part1.name}: {part1.exercises}</p>
-      <p>{part2.name}: {part2.exercises}</p>
-      <p>{part3.name}: {part3.exercises}</p>
-      <p>Number of exercises: {result}</p>
+      <Header course={course}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
